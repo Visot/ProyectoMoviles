ce999a1b94a16efbbcb94487cda8b18430a08d45
Visot <victor.sot.c@gmail.com>
2017-05-09 16:44:58 -0500

    peticion de permisos ubicacion

--- ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/MapsActivity.java
+++ ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/MapsActivity.java
@@ -1,11 +1,18 @@
 package com.example.moviles.proyectomoviles;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,8 +20,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,View.OnClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
+    private static final int MY_PERMISSION_FINE_LOCATION = 101;
     private GoogleMap mMap;
     private Toolbar supportActionBar;
 
@@ -25,32 +33,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Button login=(Button)findViewById(R.id.camara);
+        Button login = (Button) findViewById(R.id.camara);
         login.setOnClickListener(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng ctic = new LatLng(-12.016858, -77.049769);
-        LatLng biblioteca=new LatLng(-12.017008,-77.0498252);
+        LatLng biblioteca = new LatLng(-12.017008, -77.0498252);
         mMap.addMarker(new MarkerOptions().position(ctic).title("CTIC"));
         mMap.addMarker(new MarkerOptions().position(biblioteca).title("Biblioteca FC"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ctic));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+
+        } else {
+            mMap.setMyLocationEnabled(true);
+
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions,@NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED&&ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                    Toast.makeText(getApplicationContext(), "Permisos Habilitados", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Se necesita permisos de ubicacion", Toast.LENGTH_SHORT).show();
+                }
+            }break;
+
+
+        }
+    }
 
     @Override
     public void onClick(View v) {
