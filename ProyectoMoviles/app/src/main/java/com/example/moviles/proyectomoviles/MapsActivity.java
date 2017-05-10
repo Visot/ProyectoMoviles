4f6d326a778093b9bada908348834a9052988551
Visot <victor.sot.c@gmail.com>
2017-05-09 20:30:50 -0500

    agregado de imagenes iconos y spinner

--- ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/MapsActivity.java
+++ ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/MapsActivity.java
@@ -10,26 +10,43 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener,AdapterView.OnItemSelectedListener{
 
     private static final int MY_PERMISSION_FINE_LOCATION = 101;
     private GoogleMap mMap;
-    private Toolbar supportActionBar;
+    private Spinner cmbOpt;
+    ArrayAdapter<CharSequence> adapter;
+    private static final LatLng CTIC = new LatLng(-12.016858, -77.049769);
+    private static final LatLng BIBLIOTECAFC = new LatLng(-12.017008, -77.0498252);
+    private static final LatLng TIA_GRASA = new LatLng(-12.0173706,-77.0504637);
+    private static final LatLng ESTADISTICA= new LatLng(-12.0172362,-77.0505866);
+    private static final LatLng BC= new LatLng(-12.0180023,-77.0492364);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        cmbOpt = (Spinner) findViewById(R.id.spinner);
+        adapter= ArrayAdapter.createFromResource(this, R.array.opcionmap, android.R.layout.simple_list_item_1);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cmbOpt.setOnItemSelectedListener(this);
+        cmbOpt.setAdapter(adapter);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -40,11 +57,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng ctic = new LatLng(-12.016858, -77.049769);
-        LatLng biblioteca = new LatLng(-12.017008, -77.0498252);
-        mMap.addMarker(new MarkerOptions().position(ctic).title("CTIC"));
-        mMap.addMarker(new MarkerOptions().position(biblioteca).title("Biblioteca FC"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ctic));
+        mMap.addMarker(new MarkerOptions().position(CTIC).title("CTIC").icon(BitmapDescriptorFactory.fromResource(R.drawable.minilogo)));
+        mMap.addMarker(new MarkerOptions().position(BIBLIOTECAFC).title("Biblioteca FC").icon(BitmapDescriptorFactory.fromResource(R.drawable.minilibro)));
+        mMap.addMarker(new MarkerOptions().position(BC).title("Biblioteca Central").icon(BitmapDescriptorFactory.fromResource(R.drawable.minilibro)));
+        mMap.addMarker(new MarkerOptions().position(TIA_GRASA).title("Snack ciencias").icon(BitmapDescriptorFactory.fromResource(R.drawable.minicocina)));
+        mMap.addMarker(new MarkerOptions().position(ESTADISTICA).title("Oficina de estadistica"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CTIC));
+        permiso(mMap);
+
+    }
+    public void permiso(GoogleMap mMap){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
@@ -54,8 +76,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.setMyLocationEnabled(true);
 
         }
-    }
 
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions,@NonNull int[] grantResults) {
 
@@ -79,5 +101,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent intencion;
         intencion= new Intent(getApplicationContext(),Camara.class);
         startActivity(intencion );
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        if (pos == 0) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        if (pos == 1) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        if (pos == 2) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
