6077a0a73c8d110239e2b16f82a4953350c5e917
Rogger794 <rogger.valverde@uni.pe>
2017-05-31 01:05:50 -0500

    navigation y toolbar

--- ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/Main2Activity.java
+++ ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/Main2Activity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -17,52 +21,65 @@ public class Main2Activity extends AppCompatActivity implements
 
     DrawerLayout drawerLayout;
     NavigationView navView;
+    Toolbar toolbar;
+    //ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navimain2);
+        setContentView(R.layout.activity_navimain3);
         Button mapa = (Button) findViewById(R.id.Mapa);
         mapa.setOnClickListener(this);
         Button camara = (Button) findViewById(R.id.Camara);
         camara.setOnClickListener(this);
 
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         navView = (NavigationView)findViewById(R.id.navview);
 
-        navView.setNavigationItemSelectedListener(
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        // Establecemos el actionbarToggle al drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        // Llamamos a la funcion syncState para que se muestre nuestro icono del menu.
+        actionBarDrawerToggle.syncState();
+
+        if (navView != null) {
+            setupNavigation(navView);
+        }
+    }
+
+    private void setupNavigation(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
 
                         Intent intencion;
 
-                        if(menuItem.isChecked()) menuItem.setChecked(false);
+                        if (menuItem.isChecked()) menuItem.setChecked(false);
                         else menuItem.setChecked(true);
-
-                        drawerLayout.closeDrawers();
-
 
                         switch (menuItem.getItemId()) {
                             case R.id.menu_seccion_1:
-                                Toast.makeText(getApplicationContext(),"Home", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
                                 Main2Fragment fragment = new Main2Fragment();
                                 android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                                fragmentTransaction.replace(R.id.frame,fragment);
+                                fragmentTransaction.replace(R.id.frame, fragment);
                                 fragmentTransaction.commit();
-                                break;
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
 
                             case R.id.menu_seccion_2:
-                                intencion= new Intent(getApplicationContext(),Instituciones.class);
-                                startActivity(intencion );
-                                break;
+                                intencion = new Intent(getApplicationContext(), Instituciones.class);
+                                startActivity(intencion);
+                                return true;
 
                             case R.id.menu_opcion_2:
-                                intencion= new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intencion );
-                                break;
-                            default:
+                                intencion = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intencion);
                                 return true;
 
                         }
@@ -71,6 +88,17 @@ public class Main2Activity extends AppCompatActivity implements
                     }
                 });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public void onClick(View v) {
         Intent intencion;
