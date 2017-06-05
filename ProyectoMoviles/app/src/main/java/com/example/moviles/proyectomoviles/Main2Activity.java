51e354dc283233e8d28b102cb0d4f8b06eaafa77
Rogger794 <rogger.valverde@uni.pe>
2017-06-04 21:29:41 -0500

    trabajo con fragments en proceso

--- ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/Main2Activity.java
+++ ProyectoMoviles/app/src/main/java/com/example/moviles/proyectomoviles/Main2Activity.java
@@ -1,9 +1,13 @@
 package com.example.moviles.proyectomoviles;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -11,13 +15,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity implements
-        View.OnClickListener {
+public class Main2Activity extends AppCompatActivity implements FragmentOne.OnFragmentInteractionListener{
 
     DrawerLayout drawerLayout;
     NavigationView navView;
@@ -27,13 +29,36 @@ public class Main2Activity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navimain3);
-        Button mapa = (Button) findViewById(R.id.Mapa);
+        /*Button mapa = (Button) findViewById(R.id.Mapa);
         mapa.setOnClickListener(this);
         Button camara = (Button) findViewById(R.id.Camara);
-        camara.setOnClickListener(this);
+        camara.setOnClickListener(this);*/
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = FragmentOne.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         navView = (NavigationView)findViewById(R.id.navview);
@@ -58,6 +83,8 @@ public class Main2Activity extends AppCompatActivity implements
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
 
                         Intent intencion;
+                        //Fragment fragment=null;
+                        //Class fragmentClass=null;
 
                         if (menuItem.isChecked()) menuItem.setChecked(false);
                         else menuItem.setChecked(true);
@@ -65,13 +92,15 @@ public class Main2Activity extends AppCompatActivity implements
                         switch (menuItem.getItemId()) {
                             case R.id.menu_seccion_1:
                                 Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
-                                Main2Fragment fragment = new Main2Fragment();
-                                android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                                CambiaFragment(FragmentOne.class);
+                                //fragmentClass = FragmentOne.class;
+                                /*android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                                 fragmentTransaction.replace(R.id.frame, fragment);
                                 fragmentTransaction.commit();
                                 drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
+                                return true;*/
 
+                                break;
                             case R.id.menu_seccion_2:
                                 intencion = new Intent(getApplicationContext(), Instituciones.class);
                                 startActivity(intencion);
@@ -83,13 +112,35 @@ public class Main2Activity extends AppCompatActivity implements
                                 return true;
 
                         }
+/*                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
 
+                        drawerLayout.closeDrawer(GravityCompat.START);
+*/
+
+                        drawerLayout.closeDrawer(GravityCompat.START);
                         return true;
                     }
                 });
     }
 
-    @Override
+    private void CambiaFragment(Class C){
+        Fragment fragment=null;
+        Class fragmentClass=C;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+    }
+    /*@Overrides
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -97,15 +148,40 @@ public class Main2Activity extends AppCompatActivity implements
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onClick(View v) {
-        Intent intencion;
-        if(v.getId() == R.id.Mapa)
-            intencion = new Intent(getApplicationContext(), MapsActivity.class);
-        else
-            intencion = new Intent(getApplicationContext(), Camara.class);
-        startActivity(intencion);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
 }
